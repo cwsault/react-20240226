851fc894ade8fd927615d0a2e2521006669e3deb
Casey Sault <cwsault@gmail.com>
2024-02-26 18:44:47 -0500

    Display two separate counters

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Counter from './Counter';
 import './App.css';
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [counter1, setCounter1] = useState(0);
+  const [counter2, setCounter2] = useState(0);
 
   return (
     <div className='App'>
-      <Counter value={counter} onChange={setCounter} />
+      <Counter value={counter1} onChange={setCounter1} />
+      <Counter value={counter2} onChange={setCounter2} />
     </div>
   );
 };
