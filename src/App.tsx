a45b9cbb997270c134b8ed8b341157caeba0af90
Casey Sault <cwsault@gmail.com>
2024-02-26 18:50:18 -0500

    Implement total-clicks tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import Counter from './Counter';
 import './App.css';
 
 const App = () => {
   const [counter1, setCounter1] = useState(0);
   const [counter2, setCounter2] = useState(0);
+  const [totalClicks, setTotalClicks] = useState(0);
+
+  const updateCounter1 = useCallback((newValue: number) => {
+    setCounter1(newValue);
+    setTotalClicks(totalClicks + 1);
+  }, [totalClicks]);
+
+  const updateCounter2 = useCallback((newValue: number) => {
+    setCounter2(newValue);
+    setTotalClicks(totalClicks + 1);
+  }, [totalClicks]);
 
   return (
     <div className='App'>
-      <Counter value={counter1} onChange={setCounter1} />
-      <Counter value={counter2} onChange={setCounter2} />
+      <div id='total-clicks'>
+        Total clicks = {totalClicks}
+      </div>
+
+      <Counter value={counter1} onChange={updateCounter1} />
+      <Counter value={counter2} onChange={updateCounter2} />
     </div>
   );
 };
